782a813746e7bbcbafbef22cabd51b985086b797
Francisco Cejas <francejas98@gmail.com>
2023-10-06 23:39:38 -0300

    agrego page para ver usuarios

---
+++ src/app/usuarios/page.tsx
@@ -0,0 +1,52 @@
+async function obtenerUsuarios() {
+    const respuesta = await fetch("http://localhost:3000/api/usuarios", {
+      cache: "no-store",
+    });
+    const datos = await respuesta.json();
+    return datos;
+  }
+  
+  type Usuario = {
+    id: string;
+    nombre: string;
+    edad: number;
+    email: string;
+    password: string;
+    activo: boolean;
+  };
+  
+  export default async function Pagina() {
+    const usuarios = await obtenerUsuarios();
+  
+    // return (
+    //   <section>
+    //     <h2>Usuarios</h2>
+  
+    //     <ul>
+    //       {usuarios.map((usuario: Usuario) => (
+    //         <li key={usuario.id}>
+    //           {usuario.nombre} - {usuario.edad}
+    //         </li>
+    //       ))}
+    //     </ul>
+    //   </section>
+    // );
+
+    return (
+        <section>
+          <h2>Usuarios mayores de 25 años</h2>
+      
+          <ul>
+            {usuarios
+              .filter((usuario: Usuario) => usuario.edad > 25)
+              .map((usuario: Usuario) => (
+                <li key={usuario.id}>
+                  {usuario.nombre} - {usuario.edad}
+                </li>
+              ))}
+          </ul>
+        </section>
+      );
+      
+  }
+
